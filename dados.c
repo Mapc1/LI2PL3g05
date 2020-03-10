8cd4c0a9beb238d97100d7f49e9cd26fa4a187bb
Mapc1 <marcocosta579@gmail.com>
2020-03-10 18:48:07 +0000

    Adicionei 'obter_jogador_atual' e 'obter_estado_casa'.

--- dados.c
+++ dados.c
@@ -6,8 +6,8 @@ ESTADO *inicializar_estado() {
     e->num_jogadas = 0;
     e->tabela[8][8];
     e->jogadas[32];
-    e->ultima_jogada.coluna = 0;
-    e->ultima_jogada.linha = 0;
+    e->ultima_jogada.coluna = (-1);
+    e->ultima_jogada.linha = (-1);
 
     for (int i = 0; i < 8; i++)
         for (int j = 0; j < 8; j++) {
@@ -19,6 +19,20 @@ ESTADO *inicializar_estado() {
     return e;
 }
 
+int obter_jogador_atual(ESTADO *estado){
+    //Eu não sei se esta função é suposto mudar o jogador atual ou simplesmente devolver o atual
+    if(estado->jogador_atual == 1)
+        estado->jogador_atual = 2;
+    else
+        estado->jogador_atual = 1;
+    return estado->jogador_atual;
+}
+
+CASA obter_estado_casa(ESTADO *estado, COORDENADA c){
+    return (estado->tabela[c.linha][c.coluna]);
+}
+
 int jogar(ESTADO *e, COORDENADA c) {
+    //Ainda não faz nada
     printf("jogar %d %d\n", c.coluna, c.linha);
     return 1;}
