17643e943c6f1881a0606e57dedb1930295eef6a
LuisMPSilva01 <56602110+LuisMPSilva01@users.noreply.github.com>
2020-03-10 19:53:32 +0000

    Add files via upload
    Adicionei a interface mas ainda nao sei mostrar o tabuleiro

---
+++ interface.h
@@ -0,0 +1,6 @@
+#ifndef __RASTROS_INTERFACE_H__
+#define __RASTROS_INTERFACE_H__
+#define BUF_SIZE 1024
+void mostrar_tabuleiro(ESTADO *e);
+int interpretador(ESTADO *e);
+#endif //RASTROS_INTERFACE_H
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dados.h"
+#include "interface.h"
 
 
-int main() {
-    ESTADO *estado = inicializar_estado();
-    return 0;
+int main(){
+    ESTADO *e = inicializar_estado();
+    interpretador(e);
+return 0;
 }
