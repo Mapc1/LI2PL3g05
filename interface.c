0e7c9ea4cb9a0abc60b236b95d52279b9171392a
LuisMPSilva01 <56602110+LuisMPSilva01@users.noreply.github.com>
2020-03-17 21:57:13 +0000

    Tornei o jogo infinito e adicionei o prompt

--- interface.c
+++ interface.c
@@ -8,12 +8,15 @@
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
+    printf("Ensira a posicao da sua jogada.\n");
     if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0;
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
         COORDENADA coord = {*col - 'a', *lin - '1'};
+        prompt(e,*col,*lin);
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
+    interpretador(e);
 return 1;
 }
 
@@ -21,7 +24,6 @@ void mostrar_tabuleiro(ESTADO *e) {
     int i,j;
     COORDENADA c;
     for (i = -1; i < 8; i++){
-        //Imprime a linha com as letras
         if (i==(-1)){
                 printf("   ");
                 for (j = 0; j < 8; j++)
@@ -29,9 +31,7 @@ void mostrar_tabuleiro(ESTADO *e) {
                 putchar('\n');
         }
         else {
-            //Imprime os números à esquerda
             printf("%d  ",(i+1));
-            //Imprime o tabuleiro
             for (j = 0; j < 8; j++) {
                 if(i == 0 && j == 7)
                     printf("1 ");
@@ -48,3 +48,10 @@ void mostrar_tabuleiro(ESTADO *e) {
     printf("\n");
 }
 
+
+void prompt(ESTADO *e,char *col,char *lin){
+    int njogada,jogador;
+    njogada = obter_numero_jogadas(e);
+    jogador = obter_jogador_atual(e);
+    printf ("#Jogada numero %d, PLAYER %d jogou em %c%c \n",njogada,jogador,col,lin);
+}
