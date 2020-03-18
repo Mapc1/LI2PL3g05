c7f48ef68e106dad84b63f2afb6ce3b7d72ec6fe
Alexmartins01 <Alexmartins.01@hotmail.com>
2020-03-18 15:40:06 +0000

    Comandos do interpretador

--- interface.c
+++ interface.c
@@ -8,7 +8,7 @@
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
-    printf("Ensira a posicao da sua jogada.\n");
+    printf("Insira a posicao da sua jogada.\n");
     if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0;
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
         COORDENADA coord = {*col - 'a', *lin - '1'};
@@ -16,6 +16,10 @@ int interpretador(ESTADO *e) {
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
+    //(Adicionar comandos ao interpretador);
+    if (strlen(linha) == 1 && linha[0] == 'Q') return 0; //Comando de 'Quit'; Usar return?
+    if (strlen(linha) == 2 && linha[0] == 'g' && linha[1] == 'r') mostrar_tabuleiro(e); //Comando de gravar tabuleiro
+    if (strlen(linha) == 3 && linha[0] == 'l' && linha[1] == 'e' && linha[2] == 'r'); // não sei bem o que tem que fazer
     interpretador(e);
 return 1;
 }
