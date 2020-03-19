5ccb92d2d3ecc9d736300cb2339f934ac817c856
Mapc1 <marcocosta579@gmail.com>
2020-03-19 14:58:04 +0000

    Adicionei os comandos

--- interface.c
+++ interface.c
@@ -3,12 +3,29 @@
 #include "dados.h"
 #include "interface.h"
 #include "logica.h"
+#include <string.h>
+
+//Verifica se o input e um comando
+void comando(char linha[BUF_SIZE], ESTADO *estado){
+    if(strcmp(linha,"Q\n") == 0)  exit(0);
+
+    if(strcmp(linha,"GR\n") == 0) {
+        le_ficheiro();
+        printf("\nEstado do jogo guardado em 'Rastros.txt'\n\n");
+    }
+
+    if(strcmp(linha, "LER\n") == 0) {
+        escreve_ficheiro(estado);
+        printf("\nRastros.txt importado.\n\n");
+    }
+}
 
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
-    FILE *f = fopen("Rastros", "w+");
     printf("Insira a posicao da sua jogada.\n");
+
+    //Recebe o input e verifica se e uma jogada
     if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0;
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
         COORDENADA coord = {*col - 'a', *lin - '1'};
@@ -16,11 +33,8 @@ int interpretador(ESTADO *e) {
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
-    //(Adicionar comandos ao interpretador);
-    if (strlen(linha) == 1 && linha[0] == 'Q') return 0; //Comando de 'Quit'; Usar return?
-    if (strlen(linha) == 2 && linha[0] == 'g' && linha[1] == 'r') escreve_ficheiro(e); //Comando de gravar tabuleiro
-    if (strlen(linha) == 3 && linha[0] == 'l' && linha[1] == 'e' && linha[2] == 'r') le_ficheiro(); // não sei bem o que tem que fazer
-    escreve_ficheiro(e);
+
+    comando(linha, e);
     interpretador(e);
 return 1;
 }
@@ -58,5 +72,5 @@ void prompt(ESTADO *e,char *col,char *lin){
     int njogada,jogador;
     njogada = obter_numero_jogadas(e);
     jogador = obter_jogador_atual(e);
-    printf ("#Jogada numero %d, PLAYER %d jogou em %c%c \n",njogada,jogador,col,lin);
+    printf ("#Jogada numero %d, PLAYER %d jogou em %c%c\n",njogada,jogador,col,lin);
 }
