6fba7e146be2014b1db69e172a70afd30d55ac28
Marco Costa <marco@predator-h300.home>
2020-04-23 19:29:14 +0100

    Add file reading/writting

--- bot/dados.h
+++ bot/dados.h
@@ -10,9 +10,28 @@ typedef enum{
 } CASA;
 
 typedef struct{
-    int coluna,
-    int linha
-} COORDENA;
+    int coluna, linha;
+} COORDENADA;
 
-typedef CASA TAB [8][8];
+typedef struct {
+    COORDENADA jogador1;
+    COORDENADA jogador2;
+} JOGADA;
+
+typedef JOGADA JOGADAS[32];
+
+typedef struct {
+    CASA tabuleiro[8][8];
+    JOGADAS jogadas;
+    int num_jogadas;
+    int jogador;
+} ESTADO;
+
+ESTADO *inicializa_jogadas (ESTADO *estado);
+
+ESTADO *ler_ficheiro (ESTADO *estado, char *nome);
+
+void escreve_ficheiro (ESTADO *estado, char *nome);
+
+CASA obter_casa (ESTADO *estado, COORDENADA c);
 #endif //BOT_DADOS_H
--- bot/main.c
+++ bot/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "dados.h"
 
-int main() {
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("Erro!!!\nDeve inserir dois ficheiros\n");
+        exit(1);
+    }
+
+    ESTADO *estado = malloc(sizeof(ESTADO));
+    inicializa_jogadas(estado);
+
+    ler_ficheiro(estado, argv[1]);
+    escreve_ficheiro(estado, argv[2]);
 
     return 0;
 }
