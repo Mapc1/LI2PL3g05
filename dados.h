9a3e031e51a77062520ce7c5930a81770ba802ca
Alexmartins01 <61992862+Alexmartins01@users.noreply.github.com>
2020-04-29 16:25:28 +0100

    Update dados.h

--- dados.h
+++ dados.h
@@ -1,7 +1,16 @@
-#ifndef BOT_DADOS_H
-#define BOT_DADOS_H
+/**
+@file dados.h
+Definição do estado e das funções que o manipulam
+*/
 
-typedef enum{
+#ifndef __RASTROS_DADOS_H__
+#define __RASTROS_DADOS_H__
+#include <stdio.h>
+
+/**
+\brief Tipo de dados para a casa
+*/
+typedef enum {
     UM = '1',
     DOIS = '2',
     VAZIA = '.',
@@ -9,55 +18,118 @@ typedef enum{
     PRETA = '#'
 } CASA;
 
-
-typedef struct{
+/**
+\brief Tipo de dados para as coordenadas
+*/
+typedef struct {
     int coluna, linha;
 } COORDENADA;
 
+/**
+\brief Tipo de dados para a jogada
+*/
 typedef struct {
     COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
 
+/**
+\brief Tipo de dados para as jogadas
+*/
 typedef JOGADA JOGADAS[32];
 
+/**
+\brief Tipo de dados para o estado
+*/
 typedef struct {
-    CASA tabuleiro[8][8];
-    JOGADAS jogadas;
+    CASA tabela[8][8];
     COORDENADA ultima_jogada;
+    JOGADAS jogadas;
     int num_jogadas;
-    int bot;
+    int jogador_atual;
+    int undo;
 } ESTADO;
 
-typedef struct nodo {
-    COORDENADA coord;
-    struct nodo *prox;
-} Nodo, *LISTA;
+/**
+\brief Inicializa o estado do jogo
+Esta função inicializa o valor do estado. Isso implica o tabuleiro ser colocado na posição inicial e todos os campos do estado estarem com o valor por omissão.
+*/
+ESTADO *inicializar_estado(ESTADO *estado);
 
-typedef struct ramo {
-    COORDENADA coord;
-    LISTA jogadas_passadas;
-    double valor;
-    double passagens;
-    struct ramo *SE;
-    struct ramo *SM;
-    struct ramo *SD;
-    struct ramo *ME;
-    struct ramo *MD;
-    struct ramo *IE;
-    struct ramo *IM;
-    struct ramo *ID;
-} Ramo, *ARVORE;
+/**
+\brief Devolve o estado de um jogador
+@param estado Estado do jogo
+*/
+int obter_jogador_atual(ESTADO *estado);
 
-ESTADO *inicializa_jogadas (ESTADO *estado);
+/**
+\brief Devolve o número de jogadas
+@param estado do jogo
+*/
+int obter_numero_jogadas(ESTADO *estado);
+/**
+\brief Aumenta em um, o numero de jogadas
+@param estado Estado do jogo
+*/
+void aumentar_numero_jogadas(ESTADO *estado);
 
-ESTADO *ler_ficheiro (ESTADO *estado, char *nome);
+/**
+\brief 
+@param estado Estado do jogo
+@para c Coordenada atual do jogador
+*/
+int aux_jogar(ESTADO *estado, COORDENADA c, CASA casa1, CASA casa2);
 
-void escreve_ficheiro (ESTADO *estado, char *nome);
+/**
+\brief Verifica se a casa está ocupada ou vazia
+@param e Estado do jogo
+@param c Coordenada da casa
+*/
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 
-CASA obter_casa (ESTADO *estado, COORDENADA c);
+/**
+\brief Muda o jogador de casa
+@param estado Estado do jogo
+*/
+void muda_jogador(ESTADO *estado);
 
-int obter_bot(ESTADO *estado);
+/**
+\brief Atualiza a posição dos jogadores
+@param estado Estado do jogo
+@param j1 Coordenada da casa do jogador 1
+@param j2 Coordenada da casa do jogador 2
+*/
+void insere_jogada (ESTADO *estado, COORDENADA j);
 
-COORDENADA obter_ultima_jogada(ESTADO *estado);
-#endif //BOT_DADOS_H
+/**
+\brief Lê um ficheiro
+@param estado Estado do jogo
+@param s Nome do ficheiro
+Lê o estado do jogo presente no ficheiro
+*/
+ESTADO *ler_ficheiro (ESTADO *estado, char *s);
+
+/**
+\brief Guarda estado do jogo
+@param estado Estado do jogo
+@param s Nome do ficheiro
+Guarda o estado do jogo num ficheiro
+*/
+void escreve_ficheiro (ESTADO *estado, char  *s);
+
+/**
+\brief Obtem os movimentos dos jogadores
+@param estado Estado do jogo
+@param i número da jogada
+Dado um número da jogada, esta função devolve as jogadas executadas
+*/
+JOGADA obter_movs (ESTADO *estado, int i);
+
+/**
+\brief Volta à jogada atual
+@param estado Estado do jogo
+Caso não seja efetuada nenhuma jogada após a utilização do comando pos, volta a posição atual
+*/
+void reverte_estado (ESTADO *estado);
+
+#endif //RASTROS_DADOS_H
