164089c5cbb911be1e1089ec9d87ca930c8ba8e8
Alexmartins01 <61992862+Alexmartins01@users.noreply.github.com>
2020-04-29 16:26:56 +0100

    Update listas.h

--- listas.h
+++ listas.h
@@ -6,19 +6,53 @@ Definição das listas e das funções que as alteram
 #ifndef RASTROS_LISTAS_H
 #define RASTROS_LISTAS_H
 #include "listas.h"
-#include "dados.h"
 
 /**
 \brief Tipo de dados para as listas
 */
+typedef struct nodo {
+    void *v;
+    struct nodo *prox;
+} Nodo, *LISTA;
+
 
 /**
 \brief Insere uma nova cabeça na lista
 @param x nova cabeça da lista
 @param xs lista
 */
-LISTA insere_cabeca (COORDENADA x, LISTA xs);
+LISTA insere_cabeca (void *x, LISTA xs);
 
+/**
+\brief Transforma um array numa lista
+@param v Array a ser transformado
+@param N Tamanho do array
+*/
+LISTA fromArray (void *v[], int N, int tamcel);
+
+/**
+\brief Cria uma lista vazia
+*/
+LISTA criar_lista();
+
+/**
+\brief Devolve a cabeça de uma lista
+@param L Lista
+*/
+void *devolve_cabeca(LISTA L);
+
+/**
+\brief Devolve a cauda de uma lista
+@param L Lista
+*/
+LISTA proximo(LISTA L);
+
+
+/**
+\brief Remove a cabeça da lista e devolve a cauda
+@param L Lista
+*/
+LISTA remove_cabeca(LISTA L);
 
 /**
 \brief Verifica se a lista está vazia
@@ -27,10 +61,4 @@ Devolve verdadeiro (1) se estiver vazia
 */
 int lista_esta_vazia(LISTA L);
 
-COORDENADA indice_lista(LISTA lista, int indice);
-
-LISTA cloneL (LISTA l);
-int coordenadas_iguais (COORDENADA x,COORDENADA y);
-int nao_repete(LISTA l,COORDENADA x);
-int tamanhodalista (LISTA lista);
 #endif //RASTROS_LISTAS_H
